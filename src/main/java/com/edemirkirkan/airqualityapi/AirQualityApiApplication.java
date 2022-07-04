c28e11ae4dc4bb7347b3c95a5583963fabb6b9ea
edemirkirkan <ege.demirkirkan@ug.bilkent.edu.tr>
2022-07-04 07:28:12 +0300

    Initialize the project

---
+++ src/main/java/com/edemirkirkan/airqualityapi/AirQualityApiApplication.java
@@ -0,0 +1,13 @@
+package com.edemirkirkan.airqualityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirQualityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirQualityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edemirkirkan/airqualityapi/AirQualityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edemirkirkan.airqualityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirQualityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
