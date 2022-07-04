1fc8d5f0d1ef9124b0ffaebedb07efc82b9e9e4c
edemirkirkan <ege.demirkirkan@ug.bilkent.edu.tr>
2022-07-04 07:30:43 +0300

    Add user module

---
+++ src/main/java/com/edemirkirkan/airqualityapi/usr/converter/UsrUserMapper.java
@@ -0,0 +1,18 @@
+package com.edemirkirkan.airqualityapi.usr.converter;
+
+import com.edemirkirkan.airqualityapi.gen.sec.jwt.SecUserDetails;
+import com.edemirkirkan.airqualityapi.usr.dto.UsrUserResponseDto;
+import com.edemirkirkan.airqualityapi.usr.dto.UsrUserSaveRequestDto;
+import com.edemirkirkan.airqualityapi.usr.entity.UsrUser;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+public abstract class UsrUserMapper {
+
+    public abstract UsrUserResponseDto convertToUsrUserResponseDto(UsrUser usrUser);
+
+    public abstract UsrUser convertToUsrUser(SecUserDetails secUserDetails);
+
+    public abstract UsrUser convertToUsrUser(UsrUserSaveRequestDto usrUserSaveRequestDto);
+}
