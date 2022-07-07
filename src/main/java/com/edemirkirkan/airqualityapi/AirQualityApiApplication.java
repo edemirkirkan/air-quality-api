8be2802a87787bf71a662f7d04bf9cee119ccace
edemirkirkan <ege.demirkirkan@ug.bilkent.edu.tr>
2022-07-08 01:05:08 +0300

    Configure swagger path

--- src/main/java/com/edemirkirkan/airqualityapi/AirQualityApiApplication.java
+++ src/main/java/com/edemirkirkan/airqualityapi/AirQualityApiApplication.java
@@ -2,26 +2,12 @@ package com.edemirkirkan.airqualityapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
-
-import java.net.URI;
-
-import static org.springframework.web.servlet.function.RequestPredicates.GET;
-import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class AirQualityApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirQualityApiApplication.class, args);
-	}
-
-	@Bean
-	RouterFunction<ServerResponse> routerFunction() {
-		return route(GET("/index.html"), req ->
-				ServerResponse.temporaryRedirect(URI.create("/swagger-ui/index.html")).build());
 	}
 
 }
